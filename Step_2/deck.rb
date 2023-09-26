4620eeaaba36581b3711121502f441edb8d28112
DAndreevskikh <133801191+DAndreevskikh@users.noreply.github.com>
2023-09-26 20:36:28 +0300

    Create Deck

---
+++ Step_2/deck.rb
@@ -0,0 +1,23 @@
+class Deck
+  SUITS = ['<3', '<>', '*', '+'].freeze
+  RANKS = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
+
+  def initialize
+    @cards = build_deck
+    shuffle
+  end
+
+  def shuffle
+    @cards.shuffle!
+  end
+
+  def draw
+    @cards.pop
+  end
+
+  private
+
+  def build_deck
+    SUITS.product(RANKS).map { |suit, rank| Card.new(rank, suit) }
+  end
+end
