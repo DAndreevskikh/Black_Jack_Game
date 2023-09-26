d3e0fbe3f0fcf626fe007d02f4bb6d72645384a2
DAndreevskikh <133801191+DAndreevskikh@users.noreply.github.com>
2023-09-26 21:29:11 +0300

    Step_3

---
+++ player.rb
@@ -0,0 +1,37 @@
+class Player
+  attr_reader :name, :bank, :hand
+
+  def initialize(name, bank)
+    @name = name
+    @bank = bank
+    @hand = []
+  end
+
+  def add_to_hand(card)
+    @hand << card
+  end
+
+  def hand_value
+    total = @hand.sum { |card| card_value(card.rank) }
+    aces = @hand.count { |card| card.rank == 'A' }
+
+    while aces > 0 && total > 21
+      total -= 10
+      aces -= 1
+    end
+
+    total
+  end
+
+  private
+
+  def card_value(rank)
+    if %w[J Q K].include?(rank)
+      10
+    elsif rank == 'A'
+      11
+    else
+      rank.to_i
+    end
+  end
+end
